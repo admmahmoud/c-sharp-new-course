bd0b7a383f56d63248dc199fd860c8458ca8c25f
admmahmoud <admmahmoud.am@gmail.com>
2016-08-20 17:21:42 +0200

    data types float
    this is my data types float

---
+++ data types floatpointtypes/data types floatpointtypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_types_floatpointtypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flmin = float.MinValue;
+            float flmax = float.MaxValue;
+            Console.WriteLine("min value= {0} max value= {1}", flmin, flmax);
+
+
+            float flvar = 1f / 3;
+            double dlvar = 1D / 3;
+            decimal dcvar = 1M / 3;
+            Console.WriteLine("float value ={0}\n double value ={1}\n decimal value ={2}", flvar, dlvar, dcvar);
+            Console.ReadKey();
+        }
+    }
+}
