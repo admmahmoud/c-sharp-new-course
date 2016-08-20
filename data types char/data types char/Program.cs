dd0f26bb739b4242690952e46e2747867dc2366f
admmahmoud <admmahmoud.am@gmail.com>
2016-08-20 18:18:02 +0200

    data type char ,bool ,string
    this is data types char , bool , string

---
+++ data types char/data types char/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_types_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charmin = char.MinValue;
+            int charmax = char.MaxValue;
+            char myvar = 'H';
+            int myvarvalue = 'H';
+            char myvarvalue2 = Convert.ToChar(72);
+           
+            Console.WriteLine("  min char = {0} max char  = {1} my character = {2} my variable number is {3}  my conversion value is {4}" , charmin, charmax, myvar,myvarvalue,myvarvalue2);
+
+
+            string mystring = "this is my variable";
+            Console.WriteLine("my string is  {0}", mystring);
+
+            bool mybool = true;
+            Console.WriteLine("my boolean value is {0}", mybool);
+            Console.ReadKey();
+        }
+    }
+}
