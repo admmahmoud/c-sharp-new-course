9287a6f530f86e51c9f1ed8d77773c850646a8d8
admmahmoud <admmahmoud.am@gmail.com>
2016-08-20 16:29:01 +0200

    first vaiable
    this is my second prog variable

---
+++ first variable/first variable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 99;
+            Console.WriteLine("my variable value = {0}" , a);
+            Console.ReadKey();
+        }
+    }
+}
